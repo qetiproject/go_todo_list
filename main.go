b1ae7563db3b36d9cf0fc5364d7f00c4704e5a58
qetiproject <ketideveloper@gmail.com>
2026-04-08 19:09:42 +0400

    functions

--- main.go
+++ main.go
@@ -3,16 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var shortGolang = "Watch Go crash course"
-	var fullGolang = "Watch Nana's Golang Full Course"
-	var rewardDessert = "Reward myself with a donut"
+	fmt.Println("###### Welcome to our Todolist App! ######")
 
+	var shortGolang = "Watch Go crash course"
+	var fullGolang = "Watch Golang Full Course"
+	var rewardDessert = "Reward myself with a donut"
 	var taskItems = []string{shortGolang, fullGolang, rewardDessert}
 
-	fmt.Println("###### Welcome to our Todolist App! ######")
-	fmt.Println("List of my Todos")
+	printTasks(taskItems)
+	fmt.Println()
 
+	taskItems = addTask(taskItems, "Go for a run")
+	taskItems = addTask(taskItems, "Practice coding in Go")
+
+	fmt.Println("Updated list")
+	printTasks(taskItems)
+}
+
+func printTasks(taskItems []string) {
+	fmt.Println("List of my Todos")
 	for index, task := range taskItems {
 		fmt.Printf("%d: %s\n", index+1, task)
 	}
+}
+
+func addTask(taskItems []string, newTask string) []string {
+	var updatedTaskItems = append(taskItems, newTask)
+	return updatedTaskItems
 }
